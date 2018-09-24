98a0fd2b9bad2fca463b9059cf5c53857567ae11
n113 <rfedsxc@mail.ru>
2018-09-23 23:10:17 -0700

    comments added

--- src/Mainn.java
+++ src/Mainn.java
@@ -4,11 +4,11 @@ public class Mainn {
         String toFind ="шоссе";
 
         for (int i=0; i<s.length();i++){
-            if(i+toFind.length()>s.length()){
+            if(i+toFind.length()>s.length()){ //to prevent exit beyond the length of the S
                 break;
             }
             else{
-                if(toFind.equals(s.substring(i, i+toFind.length()))){
+                if(toFind.equals(s.substring(i, i+toFind.length()))){ //finder: takes parts of the target strings and compares with the finding word. Taked parts are same long as the finding word.
                     System.out.println("\n"+"I found: ");
                     System.out.print(s.substring(0,i)+" ==>"+toFind+"<== "+ s.substring(i+toFind.length()));
 
